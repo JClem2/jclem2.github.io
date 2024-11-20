28c6e0c391d603bb7b4c0507c8453a721762ab69
JClem2 <JMClem221@gmail.com>
2024-11-20 17:35:43 -0500

    Removed read more
    Ok, removed the read more as it was unnecessary and overcomplicating things.

--- scripts.js
+++ scripts.js
@@ -9,15 +9,3 @@ function darkMode() {
     const menu = document.querySelector('.nav-links');
     menu.classList.toggle('active');
 }
-
-// Function to toggle mobile project boxes
-function toggleDetails(button) {
-    const details = button.previousElementSibling; // Select the details section
-    if (details.style.display === "block") {
-        details.style.display = "none"; // Collapse
-        button.textContent = "Read More";
-    } else {
-        details.style.display = "block"; // Expand
-        button.textContent = "Read Less";
-    }
-}
