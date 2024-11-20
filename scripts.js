d10f595df5b5dfbb4bc1517e81aab15fcf6f4ac6
JClem2 <JMClem221@gmail.com>
2024-11-20 17:27:23 -0500

    Adding project mobile view

--- scripts.js
+++ scripts.js
@@ -3,9 +3,21 @@ function darkMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
   }
-  
+
 // Function to toggle mobile menu
   function toggleMenu() {
     const menu = document.querySelector('.nav-links');
     menu.classList.toggle('active');
+}
+
+// Function to toggle mobile project boxes
+function toggleDetails(button) {
+    const details = button.previousElementSibling; // Select the details section
+    if (details.style.display === "block") {
+        details.style.display = "none"; // Collapse
+        button.textContent = "Read More";
+    } else {
+        details.style.display = "block"; // Expand
+        button.textContent = "Read Less";
+    }
 }
