eff67764e6df96703412fec22d4b7c79471f6f3b
JClem2 <JMClem221@gmail.com>
2024-11-19 19:15:50 -0500

    Focus Prototype
    My first swing at trying to make a "focus" mode for projects.

--- scripts.js
+++ scripts.js
@@ -3,3 +3,18 @@ function darkMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
   }
+
+// Function to handle the "focus" effect for projects
+function toggleProject(projectId) {
+    const project = document.getElementById(projectId);
+
+    const isExpanded = project.classList.contains("expanded");
+
+    const allProjects = document.querySelectorAll(".project-box");
+
+    allProjects.forEach(box => DOMException.classList.remove("expanded"));
+
+    if(!isExpanded) {
+        project.classList.add("expanded");
+    }
+}
